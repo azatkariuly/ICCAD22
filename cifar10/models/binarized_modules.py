ca9bba9b49f4014630855a575ff10fa08a382014
azatkariuly <azat.kariuly@gmail.com>
2022-04-24 03:31:46 +0900

    initial commit

--- cifar10/models/binarized_modules.py
+++ cifar10/models/binarized_modules.py
@@ -46,10 +46,14 @@ def satmm_cuda_temp(A, X, T=64, b=8, signed=True, nbits_psum=8, step_size_psum=N
         #return
         #psum, _ = quantizeLSQ_psum(psum, step_size_psum, nbits_psum)
 
-        out = OA(torch.sum(psum_q, axis=3).squeeze().transpose(1,-1), b=12)
+        out_n = torch.sum(psum_q, axis=3).squeeze().transpose(1,-1)
+
+        out_q = OA(torch.sum(psum_q, axis=3).squeeze().transpose(1,-1), b=b)
         #out = reduce(lambda x,y: (x+y).clip(min, max), psum.transpose(0,3)).squeeze().transpose(0,-1)
         #out = OA(torch.sum(psum_q, axis=3).squeeze().transpose(1,-1), b=b)
-        #torch.save(out, 'out_n.pt')
+        torch.save(out_n, 'out_n.pt')
+        torch.save(out_q, 'out_q.pt')
+        return
         #out = cyclic_activation(out, k=2, b=b)
         return out * s #.round() #tep_size_psum
 
