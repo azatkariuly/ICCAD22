cad6809f6bb9459b0ced5f1e6c2a8c0afeb45df4
azatkariuly <azat.kariuly@gmail.com>
2022-04-27 14:10:22 +0900

    initial commit

--- cifar10/main.py
+++ cifar10/main.py
@@ -211,14 +211,12 @@ def main():
     #logging.info('training regime: %s', regime)
 
     i = 0
-    '''
     for name, param in model.named_parameters():
         if 'step_size_psum' not in name:
             param.requires_grad = False
         #param.requires_grad = False
         i += 1
-    '''
-    
+
     print('freezed', i, 'parameters')
 
     for epoch in range(args.start_epoch, args.epochs):
