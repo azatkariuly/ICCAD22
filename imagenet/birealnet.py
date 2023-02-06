8c150ede2113c566c73e5961e839c736e407fa42
azatkariuly <azat.kariuly@gmail.com>
2023-02-06 19:38:20 +0900

    initial commit

--- imagenet/birealnet.py
+++ imagenet/birealnet.py
@@ -83,15 +83,15 @@ def satmm_cuda_temp(A, X, T=64, SA=False, b=8, signed=True, nbits_psum=8, step_s
                 shift_value = 5
 
         #psum_q, s = quant_PTQ_cust(psum, nbits_psum)
-        psum_q, _ = quantizeLSQ_psum(psum, step_size_psum, nbits_psum)
-        # psum_q, _ = quantizeLSQ_psum(psum, 2**shift_value, nbits_psum)
+        # psum_q, _ = quantizeLSQ_psum(psum, step_size_psum, nbits_psum)
+        psum_q, _ = quantizeLSQ_psum(psum, 2**shift_value, nbits_psum)
 
         if SA:
             out = reduce(lambda x,y: (x+y).clip(min, max), psum_q.transpose(0,3)).squeeze().transpose(0,-1)
         else:
             out = OA(torch.sum(psum_q, axis=3).squeeze().transpose(1,-1), b=b)
         #out = cyclic_activation(out, k=2, b=b)
-        return out*step_size_psum
+        return out*(2**shift_value) #step_size_psum
     #out = reduce(lambda x,y: (x+y).clip(min, max), psum.transpose(0,3)).squeeze().transpose(0,-1)
     #out = OA(torch.sum(psum, axis=3).squeeze().transpose(1,-1), b=b)
     #return out
