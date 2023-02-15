ff635eea1100069d8b2087c57e4617b8116ae8d6
azatkariuly <azat.kariuly@gmail.com>
2023-02-15 13:13:03 +0900

    initial commit

--- cifar10/models/binarized_modules.py
+++ cifar10/models/binarized_modules.py
@@ -210,13 +210,13 @@ class BinarizeConv2d(nn.Conv2d):
             self.step_size_psum.data.copy_(20 * self.weight.abs().mean() / math.sqrt(2 ** (self.nbits_psum - 1) - 1))
             self.init_state.fill_(1)
         '''
-        out = nn.functional.conv2d(input, self.weight, None, self.stride, self.padding, self.dilation, self.groups)
+        # out = nn.functional.conv2d(input, self.weight, None, self.stride, self.padding, self.dilation, self.groups)
 
-        # out = satconv2D(input, self.weight, self.padding, self.stride,
-        #                 T=self.T, SA=self.SA, b=self.nbits_acc, signed=True,
-        #                 nbits_psum=self.nbits_psum, step_size_psum=self.step_size_psum)
+        out = satconv2D(input, self.weight, self.padding, self.stride,
+                        T=self.T, SA=self.SA, b=self.nbits_acc, signed=True,
+                        nbits_psum=self.nbits_psum, step_size_psum=self.step_size_psum)
 
-        out = OA(out.int(), b=self.nbits_acc).float() + out - out.int()
+        # out = OA(out.int(), b=self.nbits_acc).float() + out - out.int()
 
         if not self.bias is None:
             self.bias.org=self.bias.data.clone()
