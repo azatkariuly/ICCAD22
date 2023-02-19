79afaf1bc26945ab5bc95d818c0069e5e6ece5ee
azatkariuly <azat.kariuly@gmail.com>
2023-02-19 15:03:00 +0900

    initial commit

--- cifar10/models/binarized_modules.py
+++ cifar10/models/binarized_modules.py
@@ -78,6 +78,7 @@ def satmm_cuda_temp(A, X, T=64, SA=False, b=8, signed=True, nbits_psum=8, step_s
         # psum_q, s = quant_PTQ_cust(psum, nbits_psum)
         # psum_q, s = quant_PTQ(psum, step_size_psum, nbits_psum)
         # psum_q, _ = quantizeLSQ_psum(psum, step_size_psum, nbits_psum)
+        print(shift_value)
         psum_q, _ = quantizeLSQ_psum(psum, 2**shift_value, nbits_psum)
 
         if SA:
